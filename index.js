a70e710d025b38cc2dc1e2718c8498ba95701788
Roberto Busó-García <robertobuso@gmail.com>
2018-06-13 18:28:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 
 var name = "Roberto"
 var height = "74"
-console.log(parseInt(height) + 1)
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height) + 1)
